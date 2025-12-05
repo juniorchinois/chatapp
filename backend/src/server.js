185639962e7fec6515784abceae4773f341b8feb
chinois <landrychinois@icloud.com>
2025-12-04 17:37:21 -1200

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import messageRoutes from "./routes/message.route.js"
 dotenv.config();
 
 const app = express();
-const __dirname = Path.resolve();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3002;
 
